244d2a934b8779dd15e78c4027825d2b97dc684a
Thomas Orifici <thomas.orifici@gmail.com>
2024-07-09 23:52:14 -0400

    refactoring frontend / backend

---
+++ frontend/src/components/ProfileIcon.jsx
@@ -0,0 +1,20 @@
+// src/components/ProfileIcon.jsx
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../misc/AuthContext';
+import './ProfileIcon.css';
+
+const ProfileIcon = () => {
+  const { profile } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  if (!profile || !profile.images || profile.images.length == 0) return null;
+
+  return (
+    <div className="profile-icon" onClick={() => navigate('/profile')}>
+      <img src={profile.images[0].url} alt="Profile" />
+    </div>
+  );
+};
+
+export default ProfileIcon;
