23499dd02c1aed96ff555c73ba1ae61829572bd5
Thomas Orifici <thomas.orifici@gmail.com>
2024-07-11 15:35:14 -0400

    formatting fix

--- backend/src/Main.ts
+++ backend/src/Main.ts
@@ -1,31 +1,31 @@
 import express, { Express } from "express";
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config({ path: '../.env' })
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
 
-import Model from './models/Model.js'
-import WebController from './controllers/WebController.js'
-import DateController from './controllers/DateController.js'
+import Model from "./models/Model.js";
+import WebController from "./controllers/WebController.js";
+import DateController from "./controllers/DateController.js";
 
 class Main {
     static main(): void {
-        const app: Express = express()
-        const port: number = Number(process.env.PORT) || 3001
-        
-        app.use(cors())
-        app.use(express.json())
-        
+        const app: Express = express();
+        const port: number = Number(process.env.PORT) || 3001;
+
+        app.use(cors());
+        app.use(express.json());
+
         app.listen(port, () => {
-            console.log(`Server is running on port: ${port}`)
-        })
-        
-        const model: Model = new Model()
-        const web: WebController = new WebController(model)
-        const date: DateController = new DateController(model)        
+            console.log(`Server is running on port: ${port}`);
+        });
+
+        const model: Model = new Model();
+        const web: WebController = new WebController(model);
+        const date: DateController = new DateController(model);
     }
 }
 
 // Check if this script is the main module
 if (import.meta.url === new URL(import.meta.url).href) {
     Main.main();
-  }
+}
