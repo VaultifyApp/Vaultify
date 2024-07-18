a73323192e85317bbc19c79c4941f2656c55cbc9
Thomas Orifici <thomas.orifici@gmail.com>
2024-07-18 13:53:54 -0400

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import dotenv from "dotenv";
-dotenv.config({ path: "../../.env" });
+// import dotenv from "dotenv";
+// dotenv.config({ path: "../../.env" });
 
 const container = document.getElementById("root");
 const root = createRoot(container);
