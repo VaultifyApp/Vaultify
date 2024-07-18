6f8e73ed32c7e1439489ebb26d07e8286295449e
Thomas Orifici <thomas.orifici@gmail.com>
2024-07-18 15:09:42 -0400

    removed comment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import dotenv from "dotenv";
-// dotenv.config({ path: "../../.env" });
 
 const container = document.getElementById("root");
 const root = createRoot(container);
