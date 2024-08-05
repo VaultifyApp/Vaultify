a7d62a6bd4ecd9e7394e7ed5647826a7bade3cf2
Thomas Orifici <thomas.orifici@gmail.com>
2024-08-05 19:58:18 -0400

    Create Main.jsx

---
+++ frontend/src/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./Main.css";
+import App from "./App";
+
+/**
+ * loads site wide css and renders the react app.
+ */
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
