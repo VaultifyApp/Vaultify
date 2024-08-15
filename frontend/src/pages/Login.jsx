eadc77224c53e8f7789c3a2cf90153774255c3c6
Thomas Orifici <thomas.orifici@gmail.com>
2024-08-14 21:39:11 -0400

    formatting

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,7 +17,8 @@ const Login = () => {
             const params = new URLSearchParams({
                 client_id: "79e0bbb20e714c1cb35b10742723ee7a",
                 response_type: "code",
-                redirect_uri: "https://vaultifyapp.github.io/Vaultify/#/login-callback",
+                redirect_uri:
+                    "https://vaultifyapp.github.io/Vaultify/#/login-callback",
                 scope: "user-read-private user-read-email playlist-modify-public playlist-modify-private user-top-read",
             });
             window.location.href = `https://accounts.spotify.com/authorize?${params.toString()}`;
